27a8c88b69688909327a2a2900d964c90fd3ed4a
taylor-tokareff <taylortokareff@gmail.com>
2021-08-17 18:45:09 -0700

    everything works

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import CharacterList from '../characters/CharacterList';
 import Header from '../characters/Header';
+import styles from './App.css';
+import { useSelectedTheme } from '../../state/CharacterProvider';
+
+
 
 
 export default function App() {
+
+  const selectedTheme = useSelectedTheme();
+  console.log(selectedTheme);
   return (
-    <>
-      {/* <Header /> */}
+
+    <div className={
+      selectedTheme === 'light' ?
+        styles.light : styles.dark
+    }>
+
+      <Header />
       <CharacterList />
-    </>
+    </ div>
   );
 }
--- src/components/characters/Header.jsx
+++ src/components/characters/Header.jsx
@@ -1,25 +1,29 @@
 /* eslint-disable max-len */
 import React from 'react';
-// import { useSelectedTheme, useSetSelectedTheme } from '../../state/CharacterProvider';
+import { useSelectedTheme, useSetSelectedTheme } from '../../state/CharacterProvider';
+
 
 const Header = () => {
-  // const availableThemes = useSelectedTheme();
-  // const setSelectedTheme = useSetSelectedTheme();
+  const selectedTheme = useSelectedTheme();
+  const setSelectedTheme = useSetSelectedTheme();
 
 
-  // const handleChange = ({ target }) => setSelectedTheme(target.value);
-
+  const handleChange = ({ target }) => setSelectedTheme(target.value);
   return <header>
     <h1>Futurama</h1>
 
-    {/* <ul>
-      {availableThemes.map((theme) => (
-        <li key={theme}>
-          <label>{theme}</label>
-          <input type="radio" name="theme" value={theme} onChange={handleChange} />
-        </li>
-      ))}
-    </ul> */}
+    <ul>
+
+
+      <label>light
+        <input type="radio" name="theme" value="light" onChange={handleChange} />
+      </label>
+
+      <label>dark
+        <input type="radio" name="theme" value="dark" onChange={handleChange} />
+      </label>
+
+    </ul>
   </header>;
 
 };
